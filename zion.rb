afac8968ba080cb725659ff948b50a76b118d6ed
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-03-26 14:50:03 +0100

    description

--- zion.rb
+++ zion.rb
@@ -1,6 +1,6 @@
 class Zion < Formula
   version "0.0.2"
-  desc ""
+  desc "Zion provides end-to-end encryption with anonymity to create an environment for secure communication."
   homepage "http://zion244k2d5snr6uao5mxukpacqbr4z25oaji5kegjw43ypd72pri3qd.onion/"
   url "https://github.com/a1eaiactaest/zion-formula/releases/download/main/zion-0.0.2.tar.gz"
   sha256 "11d089faa1332743d4f02e199e244962fb770857fe6e2f577c2fe152f8f94e41"
