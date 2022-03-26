70c0afd1e18c25c09ff1c06b3c2d6f5cf421b999
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-03-26 14:21:13 +0100

    add tarball, homebrew requires it

--- zion.rb
+++ zion.rb
@@ -4,8 +4,8 @@
 class Zion < Formula
   desc ""
   homepage "http://zion244k2d5snr6uao5mxukpacqbr4z25oaji5kegjw43ypd72pri3qd.onion/"
-  url "http://zion244k2d5snr6uao5mxukpacqbr4z25oaji5kegjw43ypd72pri3qd.onion/gateway.zip"
-  #version ""
+  url "https://github.com/a1eaiactaest/zion-formula"
+  version "0.0.1"
   sha256 "d30a420147346c76641e6ca6843dbcba31b70ff97315235130615d690b23c7ec"
   license "WTFPL"
 
@@ -17,19 +17,10 @@ class Zion < Formula
 
   def install
     system "./configure"
-    
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test zion`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    pipe_output("#{bin}/zion-gateway -h", 0)
   end
 end
