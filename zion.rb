395b885d9deb361ba713f2e996b48d58f124aa3f
Piotrek Rybiec <piotrrybiec28@gmail.com>
2022-03-27 11:44:27 +0200

    add archive

--- zion.rb
+++ zion.rb
@@ -1,10 +1,15 @@
 class Zion < Formula
   desc "Provides E2EE gateway to create secure communication"
-  version "0.0.2"
   homepage "https://nullby1e.github.io/zion/"
-  url "https://github.com/a1eaiactaest/zion-formula/releases/download/main/zion-0.0.2.tar.gz"
+  url "https://github.com/a1eaiactaest/zion-formula/blob/master/archive/zion-0.0.2.tar.gz"
   sha256 "11d089faa1332743d4f02e199e244962fb770857fe6e2f577c2fe152f8f94e41"
   license "WTFPL"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
   depends_on "go" => :build
   depends_on "tor" => :build
   depends_on "unzip" => :build
